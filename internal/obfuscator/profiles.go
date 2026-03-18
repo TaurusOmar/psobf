94840dd8807a0e77a983507f8992ac1304c0a0eb
Omar Salazar <72287837+TaurusOmar@users.noreply.github.com>
2026-03-18 02:41:28 -0500

    Update version number for AES-256 CTR encryption

--- internal/obfuscator/profiles.go
+++ internal/obfuscator/profiles.go
@@ -47,7 +47,7 @@ Obfuscation Levels:
   3 - Base64 encoding (alternate)
   4 - GZip + Base64 compression
   5 - Script fragmentation
-  6 - AES-256 CTR encryption (NEW in v1.2)
+  6 - AES-256 CTR encryption (NEW in v.2.0.0)
 
 Transform Pipeline Options (use with -pipeline):
   iden       - Identifier morphing (use with -iden obf)
